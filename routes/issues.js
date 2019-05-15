a04146f05fe9c4ef2927f093e13b5e3765b97b61
Arth K. Gajjar <iarthstar@gmail.com>
2019-05-15 22:26:42 +0530

    Fix : Fetching Issues
    * rewritten fetching issues i.e. removed scrapping
    * added github api to fetch issues
    * refactored api response i.e. no more responding with issues array
    * update package.json i.e. remove cheerio dependency and added keywords

--- routes/issues.js
+++ routes/issues.js
@@ -11,7 +11,6 @@
 // modules import
 const express = require('express');
 const axios = require('axios');
-const cheerio = require('cheerio');
 
 const router = express.Router();
 
@@ -26,60 +25,93 @@ const router = express.Router();
  */
 router.post('/issues', (req, res) => {
     let link = req.body.link.trim();
-    link = link.endsWith('/') ? `${link}issues` : `${link}/issues`;
+    let tested = /(((http)|(https)|())(:\/\/)(www.)*)*(github.com\/)(.+)(\/)(.+)(\/*)/.exec(link);
+    link = `https://api.github.com/repos/${tested[9]}/${tested[11]}`;
 
     let issuesArray = [];
-    let links = [];
 
-    axios.get(link)
+    axios({
+            method: 'get',
+            url: link,
+            headers: {
+                'Accept': 'application/vnd.github.v3+json'
+            },
+        })
         .then(async (response) => {
-            let $ = cheerio.load(response.data);
+            let data = response.data;
 
-            let issuesStr = $(`a[href="${link.split('github.com')[1]}"]`).find('span[class="Counter"]').text();
-            let issues = parseInt(/(\d+)/g.exec(issuesStr)[0]);
+            // If Repository has issues
+            if(data.has_issues){
+                let noOfPages = Math.ceil(data.open_issues / 100);
+                let links = [];
 
-            if (issues == 0) {
+                // Generating page links to fetch issues
+                for (let index = 1; index <= noOfPages; index++) {
+                    links.push(`https://api.github.com/repositories/${data.id}/issues?page=${index}&per_page=100`);
+                }
 
+                // Fetching issues by iterating over page links
+                await Promise.all(
+                    links.map(link => {
+                        return new Promise((resolve, reject) => {
+                            axios({
+                                    method: 'get',
+                                    url: link,
+                                    headers: {
+                                        'Accept': 'application/vnd.github.v3+json'
+                                    },
+                                })
+                                .then((response) => {
+                                    issuesArray.push(...response.data);
+                                    resolve();
+                                }).catch((err) => {
+                                    resolve();
+                                });
+                        });
+                    })
+                );
+
+                issuesArray = issuesArray.filter(elem => !elem.hasOwnProperty('pull_request'));
+
+                let yesterdayTime = new Date(Date.now() - (24 * 60 * 60 * 1000));
+                let lastWeekTime = new Date(Date.now() - (7 * 24 * 60 * 60 * 1000));;
+                let yesterday = 0,
+                    lastWeek = 0,
+                    other = 0;
+
+                // Transforming issues as per requirement
+                issuesArray.forEach(elem => {
+
+                    let createdAt = new Date(elem.created_at);
+                    if (createdAt > yesterdayTime) {
+                        yesterday++;
+                    } else if (createdAt > lastWeekTime && createdAt <= yesterdayTime) {
+                        lastWeek++;
+                    } else {
+                        other++;
+                    }
+                });
+
+                // SUCCESS RESPONSE OBJECT
+                res.send({
+                    success: true,
+                    data: {
+                        yesterday,
+                        lastWeek,
+                        other
+                    }
+                });
             } else {
-                // SCRAPING FIRST PAGE
-                let issueNodes = $('div[id^=issue_]');
-                issuesArray = [...issuesArray, ...scrapeIssues($, issueNodes)];
-
-                // If Issues are more i.e. more pages
-                if (issueNodes.length != issues) {
-                    // GET PAGES 
-                    $('div[class="pagination"]').find('a[href]').not('[class="next_page"]').each((i, e) => {
-                        links.push(`https://github.com${$(e).attr('href')}`);
-                    });
-
-                    // SCRAPING PAGES
-                    await Promise.all(
-                        links.map(link => {
-                            return new Promise((resolve, reject) => {
-                                axios.get(link)
-                                    .then((response) => {
-                                        let $ = cheerio.load(response.data);
-
-                                        let issueNodes = $('div[id^=issue_]');
-                                        issuesArray = [...issuesArray, ...scrapeIssues($, issueNodes)];
-
-                                        resolve();
-                                    }).catch((err) => {
-                                        reject();
-                                    });
-                            })
-                        })
-                    );
-                }
+                // If zero issues
+                res.send({
+                    success: true,
+                    data: {
+                        yesterday: 0,
+                        lastWeek: 0,
+                        other: 0
+                    }
+                });
             }
-
-            // SUCCESS RESPONSE OBJECT
-            res.send({
-                success: true,
-                data: {
-                    issues: issuesArray
-                }
-            });
         }).catch((error) => {
             // ERROR RESPONSE OBJECT
             res.send({
@@ -95,37 +127,6 @@ router.post('/issues', (req, res) => {
 // ─────────────────────────────────────────────────────────────────────────
 //
 
-
-
-
-//
-// ───────────────────────────────────────────────────────── HELPERS ───────
-//
-
-/**
- * scrappes issues from cheerio objects array
- * @param {any} $ cheerio
- * @param {Array} issueNodes issue nodes cheerio objects array
- * @return {Array} scrapped issue objects array
- */
-function scrapeIssues($, issueNodes) {
-    let issuesArray = [];
-    issueNodes.each((i, e) => {
-        if ($(e).find('span[aria-label="Open issue"]').length) {
-            issuesArray.push({
-                id: $(e).attr('id').trim().slice(6),
-                datetime: $(e).find('relative-time').attr('datetime'),
-                desc: $(e).find('a[data-hovercard-type="issue"]').text(),
-                author: $(e).find('a[data-hovercard-type="user"]').text()
-            });
-        }
-    });
-    return issuesArray;
-}
-
-//
-// ─────────────────────────────────────────────────────────────────────────
-//
 
 /**
  * @module issues
